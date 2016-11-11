81137ade6f117adb4bf62640f9c0653449159a73
pkhrag <pkhrag@iitk.ac.in>
2016-11-11 23:43:25 +0530

    second question added

--- assign-1.py
+++ assign-1.py
@@ -11,3 +11,19 @@ def fact(n):
 
 def count_pattern(pattern, list):
     return sum(1 for x in range(len(list)) if list[x:x + len(pattern)] == pattern)
+
+
+x = [[1, 2], 1, 2, 3]
+y = ('+', ('expt', 'x', 2), ('expt', 'y', 2))
+
+
+def depth(exp, count):
+    if not isinstance(exp, (list, tuple)):
+        return 0
+    dep = 1
+    for i in range(len(exp)):
+        if isinstance(exp[i], (list, tuple)):
+            dep = max(1 + depth(exp[i], count), dep)
+    return dep
+
+# print(depth(y, 0))
